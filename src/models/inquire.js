c99f46e494b06cb0cd926cba6fa66a66eb13ff85
Al Haseeb Zafar <94920539+ALHaseebZafar@users.noreply.github.com>
2024-12-30 12:38:09 +0500

    onlinebooking changes

--- src/models/inquire.js
+++ src/models/inquire.js
@@ -1,39 +1,41 @@
 const mongoose = require("mongoose");
 
+
 const inquireSchema = new mongoose.Schema({
   professional: {
     type: mongoose.Schema.Types.ObjectId, // Reference to the Professional model
     ref: "Professional",
     required: true,
   },
-  manualBookingDetails: {
-    date: {
-      type: String, // Booking date (e.g., '2024-12-13')
-      required: true,
+  manualBookingDetails: [
+    {
+      date: {
+        type: String, // Booking date (e.g., '2024-12-13')
+      },
+      startTime: {
+        type: String, // Start time for manual booking (e.g., '10:00 AM')
+      },
+      endTime: {
+        type: String, // End time for manual booking (e.g., '11:00 AM')
+      },
     },
-    startTime: {
-      type: String, // Start time for manual booking (e.g., '10:00 AM')
-      required: true,
+  ],
+  onlineBookingDetails: [
+    {
+      date: {
+        type: String, // Booking date (e.g., '2024-12-13')
+        required: true,
+      },
+      startTime: {
+        type: String, // Start time for online booking (e.g., '10:00 AM')
+        required: true,
+      },
+      endTime: {
+        type: String, // End time for online booking (e.g., '11:00 AM')
+        required: true,
+      },
     },
-    endTime: {
-      type: String, // End time for manual booking (e.g., '11:00 AM')
-      required: true,
-    },
-  },
-  onlineBookingDetails: {
-    date: {
-      type: String, // Booking date (e.g., '2024-12-13')
-      required: true,
-    },
-    startTime: {
-      type: String, // Start time for manual booking (e.g., '10:00 AM')
-      required: true,
-    },
-    endTime: {
-      type: String, // End time for manual booking (e.g., '11:00 AM')
-      required: true,
-    },
-  },
+  ],
 });
 
 const Inquire = mongoose.model("Inquire", inquireSchema);
